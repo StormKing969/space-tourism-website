3677f0b1ebf93f4070097c16de85b3d105dbc3ce
StormKing969 <wijesinghesaj@gmail.com>
2022-04-06 15:25:30 -0400

    body-bg fixed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
-import imgBg from './useResizedImages'
 
 const Home = () => {
-  const BgImg = imgBg().current;
-  
   return (
     <main className='home_container'>
       <div className='bg_img'>
-        <img src={ BgImg } alt='background' />
       </div>
     </main>
   )
